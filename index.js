3b5d01d41e0353c6773f0ac0af39eaaff686deb9
Goran Zaharija <gzaharija@pmfst.hr>
2020-10-22 15:58:34 +0200

    Nove rute

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ const Poruka = require('./models/poruka')
 
 
 const zahtjevInfo = (req, res, next) => {
-    console.log('Metoda:', req.method)
-    console.log('Putanja:', req.path)
-    console.log('Tijelo:', req.body)
-    console.log('---')
-    next()
+  console.log('Metoda:', req.method)
+  console.log('Putanja:', req.path)
+  console.log('Tijelo:', req.body)
+  console.log('---')
+  next()
 }
 
 app.use(zahtjevInfo)
@@ -21,89 +21,98 @@ app.use(zahtjevInfo)
 
 
 let poruke = [
-    {
-        id: 4,
-        sadrzaj: 'HTML nije jednostavan',
-        vazno: true
-    },
-    {
-        id: 8,
-        sadrzaj: 'React koristi JSX sintaksu',
-        vazno: false
-    },
-    {
-        id: 13,
-        sadrzaj: 'GET i POST su najvaznije metode HTTP protokola',
-        vazno: true
-    }
+  {
+    id: 4,
+    sadrzaj: 'HTML nije jednostavan',
+    vazno: true
+  },
+  {
+    id: 8,
+    sadrzaj: 'React koristi JSX sintaksu',
+    vazno: false
+  },
+  {
+    id: 13,
+    sadrzaj: 'GET i POST su najvaznije metode HTTP protokola',
+    vazno: true
+  }
 ]
 
 app.get('/', (req, res) => {
-    res.send('<h1>Pozdrav od Express servera + nodemona</h1>')
+  res.send('<h1>Pozdrav od Express servera + nodemona</h1>')
 })
 
 app.get('/api/poruke', (req, res) => {
-    Poruka.find({}).then(rezultat => {
-        console.log(rezultat);
-        res.json(rezultat)
-    })
+  Poruka.find({}).then(rezultat => {
+    console.log(rezultat);
+    res.json(rezultat)
+  })
 })
 
 app.get('/api/poruke/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const poruka = poruke.find(p => p.id === id)
-
-    if (poruka) {
+  Poruka.findById(req.params.id)
+    .then(poruka => {
+      if (poruka) {
         res.json(poruka)
-    } else {
+      } else {
         res.status(404).end()
-    }
+      }
 
+    })
+    .catch(err => {
+      console.log("Greška pri trazenju", err);
+      res.status(500).end()
+    })
 })
-app.delete('/api/poruke/:id', (req, res) => {
-    const id = Number(req.params.id)
-    poruke = poruke.filter(p => p.id !== id)
-    res.status(204).end()
 
+app.delete('/api/poruke/:id', (req, res) => {
+  Poruka.findByIdAndRemove(req.params.id)
+    .then(result => {
+      res.status(204).end()
+    })
+    .catch(err => next(err))
 })
 
 app.put('/api/poruke/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const podatak = req.body
-    poruke = poruke.map(p => p.id !== id ? p : podatak)
-    res.json(podatak)
+  const podatak = req.body
+  const id = req.params.id
+
+  const poruka = {
+    sadrzaj: podatak.sadrzaj,
+    vazno: podatak.vazno
+  }
+
+  Poruka.findByIdAndUpdate(id,poruka, {new: true})
+  .then( novaPoruka => {
+    res.json(novaPoruka)
+  })
+  .catch(err => next(err))
 
 })
 
 app.post('/api/poruke', (req, res) => {
-    const maxId = poruke.length > 0
-        ? Math.max(...poruke.map(p => p.id))
-        : 0
+  const podatak = req.body
+  if (!podatak.sadrzaj) {
+    return res.status(400).json({ error: 'Nedostaje sadržaj poruke' })
+  }
+  const poruka = new Poruka({
+    sadrzaj: podatak.sadrzaj,
+    vazno: podatak.vazno || false,
+    datum: new Date()
+  })
 
-    const podatak = req.body
-    if (!podatak.sadrzaj) {
-        return res.status(400).json({
-            error: 'Nedostaje sadržaj poruke'
-        })
-    }
-    const poruka = {
-        sadrzaj: podatak.sadrzaj,
-        vazno: podatak.vazno || false,
-        datum: new Date(),
-        id: maxId + 1
-    }
-
-    poruke = poruke.concat(poruka)
-    res.json(poruka)
+  poruka.save().then(spremljenaPoruka => {
+    res.json(spremljenaPoruka)
+  })
 })
 
 const nepoznataRuta = (req, res) => {
-    res.status(404).send({ error: 'nepostojeca ruta' })
+  res.status(404).send({ error: 'nepostojeca ruta' })
 }
 
 app.use(nepoznataRuta)
 
-const PORT = 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Server sluša na portu ${PORT}`);
+  console.log(`Server sluša na portu ${PORT}`);
 })
